00d330f8412248832644675b7cca556773ca9af1
Aleksandar Chakarov <alexander.chakarov1@gmail.com>
2022-04-05 23:04:46 +0300

    updated the home component

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user-service.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  get isLogged(): string | null {
+    return this.userService.isLogged;
   }
+  constructor(private userService: UserService) {}
 
+  ngOnInit(): void {}
+
+  getId(): string {
+    if (localStorage.getItem('user')) {
+      const { uid } = JSON.parse(localStorage.getItem('user')!);
+      return uid;
+    } else {
+      return '';
+    }
+  }
 }
