dc59220ae67b7325090dc2368d2ad73eade8cd78
Aleksandar Chakarov <alexander.chakarov1@gmail.com>
2022-04-10 11:46:17 +0300

    updated the ICredit interface to match the DB

--- frontend/src/app/interfaces/ICredit.ts
+++ frontend/src/app/interfaces/ICredit.ts
@@ -1,6 +1,17 @@
 export class Credit {
-  due_date: Date | undefined;
+  id: number | undefined;
+  contract_time: Date | undefined;
+  create_time: Date | null | undefined;
+  discount_code: string | null | undefined;
+  due_date: Date | null | undefined;
+  forfeit_accruals_enabled: number | undefined;
   installment_days: number | undefined;
+  installments_number: number | undefined;
   principal: number | undefined;
+  utilization_time: Date | null | undefined;
   credit_status_id: number | undefined;
+  operator_id: number | null | undefined;
+  product_id: number | undefined;
+  user_id: number | null | undefined;
+  repayment_date: Date | null | undefined;
 }
