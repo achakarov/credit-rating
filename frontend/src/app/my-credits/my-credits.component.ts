ba851bda39834f0c70df19db6c9be348903e4c4c
Aleksandar Chakarov <alexander.chakarov1@gmail.com>
2022-04-16 18:54:04 +0300

    added logic for different statuses

--- frontend/src/app/my-credits/my-credits.component.ts
+++ frontend/src/app/my-credits/my-credits.component.ts
@@ -17,16 +17,16 @@ export class MyCreditsComponent implements OnInit {
     this.userService.readMyCredits(this.userId).subscribe((result: any) => {
       this.credits = result;
       result.forEach((element: { credit_status_id: any }) => {
-        if (
-          element.credit_status_id === 40 ||
-          element.credit_status_id in [4, 5, 6, 7, 8, 9, 10, 11]
-        ) {
+        if (element.credit_status_id in [4, 5, 6, 7, 8, 9, 10, 11, 38, 40]) {
           element.credit_status_id = 'Approved';
           console.log(element);
+        } else if (element.credit_status_id in [1, 2, 3]) {
+          element.credit_status_id = 'New';
         } else {
           element.credit_status_id = 'Rejected';
         }
       });
+
       // console.log(this.credits);
     });
   }
