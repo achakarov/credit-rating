64261ce1f74d386335791b519aa00e9cbebf9c2f
Aleksandar Chakarov <alexander.chakarov1@gmail.com>
2022-04-16 19:13:00 +0300

    changed the type of credit_status_id to num|string

--- frontend/src/app/my-credits/my-credits.component.ts
+++ frontend/src/app/my-credits/my-credits.component.ts
@@ -15,12 +15,12 @@ export class MyCreditsComponent implements OnInit {
 
   ngOnInit(): void {
     this.userService.readMyCredits(this.userId).subscribe((result: any) => {
-      result.forEach((element: { credit_status_id: any }) => {
+      result.forEach((element: { credit_status_id: number | string }) => {
         if (element.credit_status_id in [4, 5, 6, 7, 8, 9, 10, 11, 38, 40]) {
           element.credit_status_id = 'Approved';
-          console.log(element);
         } else if (element.credit_status_id in [1, 2, 3]) {
           element.credit_status_id = 'New';
+          console.log(element);
         } else {
           element.credit_status_id = 'Rejected';
         }
